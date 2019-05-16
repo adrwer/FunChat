310f69057c80b19bf5df50443bcc69ec7be3be67
Adrian Werimo <adrwer7@gmail.com>
2019-05-16 09:37:43 +0300

    Added the ability to send private messages

--- FunChat/Hubs/ChatHub.cs
+++ FunChat/Hubs/ChatHub.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading.Tasks;
 using System.Web;
 using Microsoft.AspNet.SignalR;
 
@@ -17,6 +18,20 @@ namespace FunChat.Hubs
         {
             var username = Context.User.Identity.Name;
             Clients.Group(room).onNewMessage(username, message, x, y);
+        }
+
+        public void SendPrivateMessage(string toUsername, string message)
+        {
+            var fromUsername = Context.User.Identity.Name;
+            Clients.Group("users/" + toUsername).onNewPrivateMessage(fromUsername, message);
+        }
+
+        public override Task OnConnected()
+        {
+            var username = Context.User.Identity.Name;
+            if (!string.IsNullOrEmpty(username))
+                Groups.Add(Context.ConnectionId, "users/" + username);
+            return base.OnConnected();
         }
     }
 }
